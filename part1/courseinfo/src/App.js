34f2156861d95d9faba0d39b645a9a95c0f78775
turisa <tesa.jurisic1@gmail.com>
2021-09-01 10:16:50 +0200

    Fix props

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,7 +7,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   );
 };
@@ -15,15 +15,21 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
 
 const Total = (props) => {
-  return <p>{props.exercises1 + props.exercises2 + props.exercises3}</p>;
+  return (
+    <p>
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
 };
 
 const App = () => {
@@ -40,23 +46,13 @@ const App = () => {
     name: 'State of a component',
     exercises: 14,
   };
+  const parts = [part1, part2, part3];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1.name}
-        exercises1={part1.exercises1}
-        part2={part2.name}
-        exercises2={part2.exercises2}
-        part3={part3.name}
-        exercises3={part3.exercises3}
-      />
-      <Total
-        exercises1={part1.exercises1}
-        exercises2={part1.exercises2}
-        exercises3={part1.exercises3}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
