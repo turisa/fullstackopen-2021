f6732104f68d4c90e75ce10d289c7325933c6e99
turisa <tesa.jurisic1@gmail.com>
2021-09-01 15:37:10 +0200

    Solve step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 const Statistics = ({ good, bad, neutral }) => {
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return <h1>No feedback given</h1>;
+  }
   return (
     <div>
       <h1>statistics</h1>
@@ -15,7 +18,6 @@ const Statistics = ({ good, bad, neutral }) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
