27e08e28db03123f80634f6eb7f7775e12730050
turisa <tesa.jurisic1@gmail.com>
2021-09-06 11:58:19 +0200

    Refactor

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,30 +1,23 @@
 import axios from 'axios';
 
-const baseUrl = 'https://locahost:3001/persons';
+const baseUrl = 'http://localhost:3001/persons';
 
-const getAll = () => {
-  return axios.get(baseUrl).then((response) => response.data);
-};
+const getAll = () => axios.get(baseUrl).then((response) => response.data);
 
-const create = (personObject) => {
-  return axios.post(baseUrl, personObject).then((response) => response.data);
-};
+const create = (personObject) =>
+  axios.post(baseUrl, personObject).then((response) => response.data);
 
-const update = (id, personObject) => {
-  return axios
-    .put(`${baseUrl}/${id}`, personObject)
-    .then((response) => response.data);
-};
+const update = (id, personObject) =>
+  axios.put(`${baseUrl}/${id}`, personObject).then((response) => response.data);
 
-const del = (id) => {
-  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
-};
+const remove = (id) =>
+  axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
 
 const PersonService = {
   getAll,
   create,
   update,
-  del,
+  remove,
 };
 
 export default PersonService;
