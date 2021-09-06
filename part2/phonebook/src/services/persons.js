1ce672a45010ca0d4a6d7e8580dd6a828d044faf
turisa <tesa.jurisic1@gmail.com>
2021-09-06 11:36:25 +0200

    Add services

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const baseUrl = 'https://locahost:3001/persons';
+
+const getAll = () => {
+  return axios.get(baseUrl).then((response) => response.data);
+};
+
+const create = (personObject) => {
+  return axios.post(baseUrl, personObject).then((response) => response.data);
+};
+
+const update = (id, personObject) => {
+  return axios
+    .put(`${baseUrl}/${id}`, personObject)
+    .then((response) => response.data);
+};
+
+const del = (id) => {
+  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
+};
+
+const PersonsService = {
+  getAll,
+  create,
+  update,
+  del,
+};
+
+export default PersonsService;
