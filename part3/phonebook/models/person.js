c45afb5fc75a24a6a0c82389cc733bdabb69b304
turisa <tesa.jurisic1@gmail.com>
2021-09-12 13:32:48 +0200

    Update schema

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI;
 
@@ -10,8 +11,8 @@ mongoose
   .catch((error) => console.log('error connecting to mongoDB'));
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: { type: String, minLength: 3, required: true, unique: true },
+  number: { type: String, minLength: 8, required: true },
 });
 
 personSchema.set('toJSON', {
