840410acacfcf8ac4f4c069171bad92523ae07dd
turisa <tesa.jurisic1@gmail.com>
2021-09-12 14:05:47 +0200

    Solve step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -52,22 +52,17 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
-
-  if (
-    !body.name ||
-    !body.number ||
-    persons.find((person) => person.name === body.name)
-  ) {
-    return response.status(400).end();
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   });
-  person.save().then((savedPerson) => response.json(savedPerson));
+  person
+    .save()
+    .then((savedPerson) => response.json(savedPerson))
+    .catch((error) => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -91,7 +86,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.response.data });
   }
 
   next(error);
