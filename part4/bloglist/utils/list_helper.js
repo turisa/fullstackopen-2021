472f7020e2bb44bf4195c3d4506d77fe1abd74d4
turisa <tesa.jurisic1@gmail.com>
2021-09-15 19:36:36 +0200

    Add list helper

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogPosts) => {
+  const likes = blogPosts.reduce((a, b) => a.likes + b.likes, { likes: 0 });
+  return likes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
