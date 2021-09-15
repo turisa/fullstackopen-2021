e2787f1fb222801b603c20c9750598e9abcdd1ea
turisa <tesa.jurisic1@gmail.com>
2021-09-15 22:13:23 +0200

    Add helper

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,6 +5,7 @@ const dummy = (blogs) => {
 const favoriteBlog = (blogList) => {
   const maxLikes = Math.max(...blogList.map((blog) => blog.likes));
   const blog = blogList.find((blog) => blog.likes === maxLikes);
+
   return blog;
 };
 
@@ -12,7 +13,21 @@ const totalLikes = (blogList) => {
   const likes = blogList.reduce((sum, blog) => {
     return sum + blog.likes;
   }, 0);
+
   return likes;
+};
+
+const mostBlogs = (blogList) => {
+  const authorBlogCount = {};
+
+  blogList.forEach((blog) => authorBlogCount[blog.author]++);
+  const author = Math.max(...Object.values(authorBlogCount));
+
+  return author;
+};
+
+const mostLikes = (blogList) => {
+  //todo
 };
 
 module.exports = {
