988c74ed01e75162769cbf02d402c3728959fad8
turisa <tesa.jurisic1@gmail.com>
2021-09-16 16:01:32 +0200

    Fix connection

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,6 +1,16 @@
 const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+
+const mongoUrl = config.MONGODB_URI;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch((error) => logger.error(error));
 
 const app = express();
 
