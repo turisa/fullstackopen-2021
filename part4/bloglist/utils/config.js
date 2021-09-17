35168c1825604900e5b0322da641ea7f0ce86c77
turisa <tesa.jurisic1@gmail.com>
2021-09-17 17:00:48 +0200

    Update config

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+const MONGODB_URI =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : process.env.TEST_MONGODB_URI;
+
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 
