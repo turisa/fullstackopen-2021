1a4846bfa31c968dddd4150170417fcd57f14e4b
turisa <tesa.jurisic1@gmail.com>
2021-09-17 15:35:21 +0200

    Fix app

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config');
 const logger = require('./utils/logger');
 
 const mongoUrl = config.MONGODB_URI;
