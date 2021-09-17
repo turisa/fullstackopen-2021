68e9936bd551754f6b6da0743617b322e0f9379f
turisa <tesa.jurisic1@gmail.com>
2021-09-17 15:34:28 +0200

    Add tests

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+  },
+  {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  await Blog.insertMany(initialBlogs);
+});
+
+test('5 blogs are returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsReturned = response.body;
+
+  expect(blogsReturned.length).toBe(5);
+});
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
