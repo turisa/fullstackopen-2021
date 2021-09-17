f6a92b71b0028b45bb5bc1995ba2bb258dbe7bb8
turisa <tesa.jurisic1@gmail.com>
2021-09-17 17:01:53 +0200

    Update scripts

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -5,7 +5,6 @@ const MONGODB_URI =
     ? process.env.MONGODB_URI
     : process.env.TEST_MONGODB_URI;
 
-const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 
 module.exports = {
