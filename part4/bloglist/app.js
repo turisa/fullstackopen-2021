6705264adc5184d4f0ea67e1db2d47fab78cfe05
turisa <tesa.jurisic1@gmail.com>
2021-09-19 22:49:24 +0200

    Use token extractor

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/blogs', blogsRouter);
+app.use('/api/blogs', middleware.tokenExtractor, blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
