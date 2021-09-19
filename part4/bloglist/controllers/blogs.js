1ae994e17d2d00e53d9e515f10c11eac87d1e5a0
turisa <tesa.jurisic1@gmail.com>
2021-09-19 22:49:50 +0200

    Remove unused function

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -4,16 +4,6 @@ const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-
-  return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
 
@@ -33,7 +23,7 @@ blogsRouter.put('/:id', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const token = getTokenFrom(request);
+  const token = request['token'];
   const decodedToken = jwt.verify(token, process.env.SECRET);
 
   if (!token || !decodedToken.id) {
@@ -67,7 +57,7 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const token = getTokenFrom(request);
+  const token = request.token;
   const decodedToken = jwt.verify(token, process.env.SECRET);
 
   if (!token || !decodedToken.id) {
