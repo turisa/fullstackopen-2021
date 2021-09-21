6bb3849dc7714fdc16596490cdf6ad3602e938f2
turisa <tesa.jurisic1@gmail.com>
2021-09-21 23:01:39 +0200

    Enable likes

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,51 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react';
+import blogService from '../services/blogs';
 
-export default Blog
+const Blog = ({ blog, blogs, setBlogs }) => {
+  const [view, setView] = useState(false);
+
+  const buttonLabel = view ? 'hide' : 'view';
+  const showWhenView = { display: view ? '' : 'none' };
+
+  const handleToggleView = () => {
+    setView(!view);
+  };
+
+  const handleUpvote = async () => {
+    const newBlog = { ...blog, likes: blog.likes + 1 };
+    try {
+      await blogService.update(newBlog);
+
+      setBlogs(blogs.map((blog) => (blog.id === newBlog.id ? newBlog : blog)));
+    } catch (exception) {
+      // later
+    }
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={handleToggleView}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenView}>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes}
+          <button onClick={handleUpvote}>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
