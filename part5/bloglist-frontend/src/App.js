89db545a3839c3e806e6eb175814add6fdf1546b
turisa <tesa.jurisic1@gmail.com>
2021-09-21 23:04:02 +0200

    Update props

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -22,6 +22,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
+    // happens if page is refreshed
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
 
     if (loggedUserJSON) {
@@ -36,16 +37,18 @@ const App = () => {
     <LoginForm setUser={setUser} />
   ) : (
     <div>
-      <h2>blogs</h2>
+      <h1>blogs</h1>
       <p>
         {user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>
       <Togglable buttonLabel="create new blog">
         <BlogForm blogs={blogs} setBlogs={setBlogs} />
       </Togglable>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />
+        ))}
     </div>
   );
 };
