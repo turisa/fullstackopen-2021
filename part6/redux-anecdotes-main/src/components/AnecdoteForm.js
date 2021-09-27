ca517f30e2050bcb0deeeb50b38973e1b53bcdcd
turisa <tesa.jurisic1@gmail.com>
2021-09-27 11:12:46 +0200

    Update create method

--- part6/redux-anecdotes-main/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes-main/src/components/AnecdoteForm.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 
 import { createAnecdote } from '../reducers/anecdoteReducer';
+import anecdoteService from '../services/anecdotes';
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
-  const create = (event) => {
+  const create = async (event) => {
     event.preventDefault();
 
     const content = event.target.anecdote.value;
     event.target.anecdote.value = '';
 
-    dispatch(createAnecdote(content));
+    const data = await anecdoteService.createNew(content);
+    dispatch(createAnecdote(data));
   };
 
   return (
