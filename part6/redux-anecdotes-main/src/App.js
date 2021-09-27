7c9fdf3d74510bc2b07b52e0d668af93a03385aa
turisa <tesa.jurisic1@gmail.com>
2021-09-27 12:10:31 +0200

    Update reducer

--- part6/redux-anecdotes-main/src/App.js
+++ part6/redux-anecdotes-main/src/App.js
@@ -3,16 +3,11 @@ import { useDispatch } from 'react-redux';
 
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdotesList from './components/AnecdoteList';
-import anecdoteService from './services/anecdotes';
 import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
   const dispatch = useDispatch();
-
-  useEffect(() => {}, []);
-  anecdoteService
-    .getAll()
-    .then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)));
+  useEffect(() => dispatch(initializeAnecdotes()), [dispatch]);
 
   return (
     <div>
--- part6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes';
+
 const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
     case 'VOTE':
@@ -21,10 +23,13 @@ const anecdoteReducer = (state = [], action) => {
   }
 };
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes,
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    });
   };
 };
 
