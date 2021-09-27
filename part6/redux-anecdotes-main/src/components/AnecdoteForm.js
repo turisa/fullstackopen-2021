1b0c375a95ba0258a945cca86ea80a656a5296ea
turisa <tesa.jurisic1@gmail.com>
2021-09-27 09:54:45 +0200

    Refactor

--- part6/redux-anecdotes-main/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes-main/src/components/AnecdoteForm.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 
 import { createAnecdote } from '../reducers/anecdoteReducer';
-import { setNotification } from '../reducers/notificationReducer';
 
-const AnecdoteForm = (props) => {
+const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
   const create = (event) => {
