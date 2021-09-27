f0759a6de561e9ae5188167a949be645deb8bcc6
turisa <tesa.jurisic1@gmail.com>
2021-09-27 11:05:46 +0200

    Fetch anecdotes from db

--- part6/redux-anecdotes-main/src/App.js
+++ part6/redux-anecdotes-main/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdotesList from './components/AnecdoteList';
+import anecdoteService from './services/anecdotes';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {}, []);
+  anecdoteService
+    .getAll()
+    .then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)));
+
   return (
     <div>
       <AnecdotesList />
