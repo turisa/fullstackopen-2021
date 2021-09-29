3555e89c7e72e5f3b77ad4929d14751b63bc9b08
turisa <tesa.jurisic1@gmail.com>
2021-09-29 15:56:02 +0200

    Remove unused hook

--- part7/routed-anecdotes/src/index.js
+++ part7/routed-anecdotes/src/index.js
@@ -1,21 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-export const useField = (type) => {
-  const [value, setValue] = useState('');
-
-  const onChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  return {
-    type,
-    value,
-    onChange,
-  };
-};
 
 ReactDOM.render(
   <Router>
