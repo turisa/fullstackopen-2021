e52d46d5c808099c3473e55bc7ea5035a7cae10b
turisa <tesa.jurisic1@gmail.com>
2021-10-03 21:48:06 +0200

    Combine reducers

--- part7/bloglist/src/store.js
+++ part7/bloglist/src/store.js
@@ -1,8 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import blogReducer from './reducers/blogReducer';
+import userReducer from './reducers/userReducer';
 
-const store = createStore(blogReducer, applyMiddleware(thunk));
+const reducer = combineReducers({ blogs: blogReducer, user: userReducer });
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
