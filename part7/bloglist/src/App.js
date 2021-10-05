edfb4d359adfde0644921d63ad972a2b8e712c1b
turisa <tesa.jurisic1@gmail.com>
2021-10-05 09:28:43 +0200

    Remove unused imports

--- part7/bloglist/src/App.js
+++ part7/bloglist/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Blog from './components/Blog';
