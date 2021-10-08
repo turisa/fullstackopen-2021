b3fc2bdb84e462451c6cbf4d4a8ec2aa0a792753
turisa <tesa.jurisic1@gmail.com>
2021-10-08 13:15:40 +0200

    Enable author update

--- part8/library-frontend/src/components/BirthYearForm.js
+++ part8/library-frontend/src/components/BirthYearForm.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+
+import { ALL_AUTHORS, UPDATE_AUTHOR } from '../queries';
 
 const BirthYearForm = () => {
   const [name, setName] = useState('');
   const [born, setBorn] = useState('');
+
+  const [updateAuthor] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+    onCompleted: () => {
+      setName('');
+      setBorn('');
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -12,16 +26,23 @@ const BirthYearForm = () => {
     setBorn(event.target.value);
   };
 
+  const submit = (event) => {
+    event.preventDefault();
+
+    updateAuthor({ variables: { name, setBornTo: Number(born) } });
+  };
+
   return (
     <div>
       <h3>set birthyear</h3>
-      <form>
+      <form onSubmit={submit}>
         <div>
           name <input onChange={handleNameChange} value={name} />
         </div>
         <div>
           born <input onChange={handleBornChange} value={born} type="number" />
         </div>
+        <button type="submit">submit</button>
       </form>
     </div>
   );
