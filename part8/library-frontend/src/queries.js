2420389874d829a74fe22ea79d5ac72840137772
turisa <tesa.jurisic1@gmail.com>
2021-10-08 12:58:43 +0200

    Add mutation for updating author

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -43,3 +43,14 @@ export const CREATE_BOOK = gql`
     }
   }
 `;
+
+export const UPDATE_AUTHOR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`;
