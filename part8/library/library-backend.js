b02f1b5114715ff4d116fb3bf89f669108795222
turisa <tesa.jurisic1@gmail.com>
2021-10-11 12:15:47 +0200

    Fix resolvers

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -19,84 +19,6 @@ mongoose
     console.log('error connection to MongoDB:', error.message);
   });
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963,
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821,
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-];
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design'],
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns'],
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design'],
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime'],
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution'],
-  },
-];
-
 const typeDefs = gql`
   type Author {
     name: String!
@@ -108,9 +30,9 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: String!
-    id: ID!
+    author: Author!
     genres: [String!]!
+    id: ID!
   }
 
   type Query {
@@ -133,57 +55,20 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    authorCount: () => authors.length,
-    bookCount: () => books.length,
+    authorCount: () => Author.find({}).length,
+    bookCount: () => Book.find({}).length,
     allBooks: (root, args) => {
-      return;
-      /*
-      if (args.author) {
-        return books.filter((book) => book.author === args.author);
-      }
-
-      if (args.genre) {
-        return books.filter((book) => book.genres.includes(args.genre));
-      }
-      */
-      return books;
+      return Book.find({});
     },
     allAuthors: () => {
-      return authors.map((author) => ({
-        ...author,
-        bookCount: books.filter((book) => book.author === author.name).length,
-      }));
+      return Author.find({});
     },
   },
 
   Mutation: {
     addBook: (root, args) => {
-      const book = { ...args, id: uuid() };
-      books = books.concat(book);
-
-      const authorExists = authors.find(
-        (author) => author.name === args.author
-      );
-
-      if (!authorExists) {
-        const newAuthor = { name: args.author, id: uuid(), born: null };
-        authors = authors.concat(newAuthor);
-      }
-
-      return book;
-    },
-    editAuthor: (root, args) => {
-      const author = authors.find((author) => author.name === args.name);
-
-      if (!author) {
-        return null;
-      }
-
-      const newAuthor = { ...author, born: args.setBornTo };
-
-      authors = authors.map((author) =>
-        author.name === newAuthor.name ? newAuthor : author
-      );
+      const author = new Author(args);
+      const newAuthor = author.save();
 
       return newAuthor;
     },
